82f9ca253a8649bd468ba0a49d2ee484f7471d56
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-05-19 13:26:32 +0800

    remove spell-checker and make config more generic

--- jquery.js
+++ jquery.js
@@ -1,26 +1,15 @@
 /* eslint-env node */
 /* eslint-disable quote-props */
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const spellcheckRule = require('./spell-check').rules['spellcheck/spell-checker'];
-
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module',
-    'ecmaVersion': 2018,
-  },
   plugins: [
-    'spellcheck',
     'no-only-tests',
   ],
+  extends: [
+    'eslint:recommended',
+    'devextreme/javascript',
+  ],
   rules: {
-    'spellcheck/spell-checker': spellcheckRule,
     'class-methods-use-this': 0, // TODO warn (was error)
     'func-names': 0, // TODO warn (was warn) >500
     'import/extensions': 0,
@@ -28,43 +17,9 @@ module.exports = {
     'prefer-destructuring': 0,
     'no-param-reassign': ['error', { 'props': false }],
     'no-only-tests/no-only-tests': 'error',
+    'quote-props': ['error', 'consistent'],
+    'no-dupe-keys': 0,
+    'no-unused-vars': 0,
+    'no-undef': 0,
   },
-  extends: [
-    'eslint:recommended',
-    'devextreme/javascript',
-    'devextreme/spell-check',
-  ],
-  overrides: [{
-    files: [
-      '*.js',
-    ],
-    globals: {
-      System: true,
-    },
-    rules: {
-      'quote-props': ['error', 'consistent'],
-      'no-dupe-keys': 0,
-    },
-  }, {
-    files: [
-      '*.*',
-    ],
-    env: {
-      jquery: true,
-    },
-    globals: {
-      DevExpress: true,
-    },
-    overrides: [{
-      files: '**/data.js',
-      rules: {
-        'no-unused-vars': 0,
-      },
-    }, {
-      files: '**/index.js',
-      rules: {
-        'no-undef': 0,
-      },
-    }],
-  }],
 };
--- react.js
+++ react.js
@@ -1,26 +1,17 @@
 /* eslint-env node */
 /* eslint-disable quote-props */
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const spellcheckRule = require('./spell-check').rules['spellcheck/spell-checker'];
-
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es6: true,
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module',
-    'ecmaVersion': 2018,
-  },
   plugins: [
-    'spellcheck',
     'no-only-tests',
+    'react-perf'
+  ],
+  extends: [
+    'eslint:recommended',
+    'devextreme/typescript',
+    'plugin:react/recommended'
   ],
   rules: {
-    'spellcheck/spell-checker': spellcheckRule,
     'class-methods-use-this': 0, // TODO warn (was error)
     'func-names': 0, // TODO warn (was warn) >500
     'import/extensions': 0,
@@ -28,180 +19,133 @@ module.exports = {
     'prefer-destructuring': 0,
     'no-param-reassign': ['error', { 'props': false }],
     'no-only-tests/no-only-tests': 'error',
-  },
-  extends: [
-    'eslint:recommended',
-    'devextreme/javascript',
-    'devextreme/spell-check',
-  ],
-  overrides: [{
-    'files': [
-      '*.ts', '*.tsx',
-    ],
-    extends: ['devextreme/typescript'],
-    parserOptions: {
-      project: './tsconfig.json',
-      'createDefaultProgram': true,
-      'ecmaVersion': 6,
-    },
-    rules: {
-      // TODO consider this rules
-      'eqeqeq': 0,
-      'no-plusplus': 0,
-      'max-len': 0,
-      'max-classes-per-file': 0,
-      'consistent-return': 0,
-      'import/extensions': 0,
-      'import/no-webpack-loader-syntax': 0,
-      'no-restricted-properties': 0,
-      'no-restricted-globals': 0,
-      'spellcheck/spell-checker': 0,
-      'no-mixed-operators': 0,
-      'no-useless-concat': 0,
-      'no-self-assign': 0,
-      'default-case': 0,
-      'no-var': 0,
-      'no-return-assign': 0,
-      'vars-on-top': 0,
-      'no-sequences': 0,
-      'new-cap': 0,
-      'no-param-reassign': 0,
-      'no-multi-assign': 0,
-      'no-restricted-syntax': 0,
-      'prefer-rest-params': 0,
-      'radix': 0,
-      'no-underscore-dangle': 0,
-      'operator-assignment': 0,
-      'prefer-const': 0,
-      '@typescript-eslint/naming-convention': 0,
-      '@typescript-eslint/no-throw-literal': 0,
-      '@typescript-eslint/no-use-before-define': 0,
-      '@typescript-eslint/no-shadow': 0,
-      '@typescript-eslint/no-loop-func': 0,
-      '@typescript-eslint/ban-types': 0,
-      '@typescript-eslint/no-this-alias': 0,
-      '@typescript-eslint/no-empty-function': 0,
-      '@typescript-eslint/no-explicit-any': 0,
-      '@typescript-eslint/no-unused-vars': 0,
-      '@typescript-eslint/no-unused-expressions': 0,
-      '@typescript-eslint/no-useless-constructor': 0,
-      '@typescript-eslint/explicit-module-boundary-types': 0, // was warn
-
-    },
-  }, {
-    files: [
-      '*.*',
-    ],
-    extends: [
-      'plugin:react/recommended',
-    ],
-    plugins: [
-      'react-perf',
-    ],
-    globals: {
-      System: false,
-      AzureGateway: false,
-      AzureFileSystem: false,
-    },
-    settings: {
-      react: {
-        createClass: 'createReactClass',
-        'pragma': 'React',
-        version: '16.2',
-        flowVersion: '0.53',
+    'eqeqeq': 0,
+    'no-plusplus': 0,
+    'max-len': 0,
+    'max-classes-per-file': 0,
+    'consistent-return': 0,
+    'import/extensions': 0,
+    'import/no-webpack-loader-syntax': 0,
+    'no-restricted-properties': 0,
+    'no-restricted-globals': 0,
+    'spellcheck/spell-checker': 0,
+    'no-mixed-operators': 0,
+    'no-useless-concat': 0,
+    'no-self-assign': 0,
+    'default-case': 0,
+    'no-var': 0,
+    'no-return-assign': 0,
+    'vars-on-top': 0,
+    'no-sequences': 0,
+    'new-cap': 0,
+    'no-param-reassign': 0,
+    'no-multi-assign': 0,
+    'no-restricted-syntax': 0,
+    'prefer-rest-params': 0,
+    'radix': 0,
+    'no-underscore-dangle': 0,
+    'operator-assignment': 0,
+    'prefer-const': 0,
+    '@typescript-eslint/naming-convention': 0,
+    '@typescript-eslint/no-throw-literal': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/no-loop-func': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/no-this-alias': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-unused-expressions': 0,
+    '@typescript-eslint/no-useless-constructor': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0, // was warn
+    'block-spacing': 'error',
+    'comma-spacing': 'error',
+    'computed-property-spacing': 'error',
+    'comma-style': ['error', 'last'],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-irregular-whitespace': 'error',
+    'no-multi-spaces': 'error',
+    'no-trailing-spaces': 'error',
+    'no-new-func': 'error',
+    'no-eval': 'error',
+    'no-undef': 'error',
+    'no-unused-expressions': 'off',
+    'no-unused-vars': ['error'],
+    'no-extend-native': 'error',
+    'no-alert': 'error',
+    'no-whitespace-before-property': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'semi-spacing': 'error',
+    semi: 'error',
+    'space-before-blocks': 'error',
+    'space-before-function-paren': ['error', 'never'],
+    'space-in-parens': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'eol-last': ['error', 'always'],
+    curly: ['error', 'multi-line', 'consistent'],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        MemberExpression: 1,
+        CallExpression: {
+          arguments: 1,
+        },
       },
-      propWrapperFunctions: [
-        'forbidExtraProps',
-      ],
-    },
-    rules: {
-      'block-spacing': 'error',
-      'comma-spacing': 'error',
-      'computed-property-spacing': 'error',
-      'comma-style': ['error', 'last'],
-      'no-multiple-empty-lines': ['error', { max: 1 }],
-      'no-irregular-whitespace': 'error',
-      'no-multi-spaces': 'error',
-      'no-trailing-spaces': 'error',
-      'no-new-func': 'error',
-      'no-eval': 'error',
-      'no-undef': 'error',
-      'no-unused-expressions': 'off',
-      'no-unused-vars': ['error'],
-      'no-extend-native': 'error',
-      'no-alert': 'error',
-      'no-whitespace-before-property': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'semi-spacing': 'error',
-      semi: 'error',
-      'space-before-blocks': 'error',
-      'space-before-function-paren': ['error', 'never'],
-      'space-in-parens': 'error',
-      'space-infix-ops': 'error',
-      'space-unary-ops': 'error',
-      'eol-last': ['error', 'always'],
-      curly: ['error', 'multi-line', 'consistent'],
-      indent: [
-        'error',
-        2,
-        {
-          SwitchCase: 1,
-          MemberExpression: 1,
-          CallExpression: {
-            arguments: 1,
-          },
-        },
-      ],
-      quotes: ['error', 'single', { avoidEscape: true }],
-      'prefer-template': 'error',
-      'func-style': ['error', 'declaration'],
-      'react/jsx-curly-brace-presence': [
-        'error',
-        {
-          props: 'never',
-          children: 'never',
-        },
-      ],
-      'react/jsx-fragments': ['error', 'element'],
-      'react/jsx-no-bind': [
-        'error',
-        {
-          allowBind: true, // TODO false (was false)
-          allowArrowFunctions: true, // TODO false (was false)
-          ignoreRefs: true,
-        },
-      ],
-      'react/jsx-tag-spacing': [
-        'error',
-        {
-          beforeClosing: 'never',
-        },
-      ],
-      'react/jsx-no-undef': [
-        'error',
-        {
-          allowGlobals: true,
-        },
-      ],
-      'react/prop-types': 'off',
-      'react/jsx-no-target-blank': [
-        'error',
-        {
-          enforceDynamicLinks: 'never',
-        },
-      ],
-      'react-perf/jsx-no-new-object-as-prop': [
-        'error',
-        {
-          nativeAllowList: 'all',
-        },
-      ],
-      'react-perf/jsx-no-new-array-as-prop': [
-        'error',
-        {
-          nativeAllowList: 'all',
-        },
-      ],
-    },
-  }],
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'prefer-template': 'error',
+    'func-style': ['error', 'declaration'],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      {
+        props: 'never',
+        children: 'never',
+      },
+    ],
+    "react/jsx-filename-extension": [1, { "extensions": [".ts", ".tsx"] }],
+    'react/jsx-fragments': ['error', 'element'],
+    'react/jsx-no-bind': [
+      'error',
+      {
+        allowBind: true, // TODO false (was false)
+        allowArrowFunctions: true, // TODO false (was false)
+        ignoreRefs: true,
+      },
+    ],
+    'react/jsx-tag-spacing': [
+      'error',
+      {
+        beforeClosing: 'never',
+      },
+    ],
+    'react/jsx-no-undef': [
+      'error',
+      {
+        allowGlobals: true,
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/jsx-no-target-blank': [
+      'error',
+      {
+        enforceDynamicLinks: 'never',
+      },
+    ],
+    'react-perf/jsx-no-new-object-as-prop': [
+      'error',
+      {
+        nativeAllowList: 'all',
+      },
+    ],
+    'react-perf/jsx-no-new-array-as-prop': [
+      'error',
+      {
+        nativeAllowList: 'all',
+      },
+    ],
+  },
 };
