b06190e88406cbb1d11ede9db055d1d827a49548
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-06-01 14:13:09 +0800

    disable no-undef

--- vue.js
+++ vue.js
@@ -67,7 +67,7 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'no-new-func': 'error',
     'no-eval': 'error',
-    'no-undef': 'error',
+    'no-undef': 'off',
     'no-unused-expressions': 'off',
     'no-unused-vars': ['error'],
     'no-extend-native': 'error',
