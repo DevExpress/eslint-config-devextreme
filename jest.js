eeedb6560604af6ea2fe4fb038603338aa110b92
Alyar <>
2025-09-30 11:49:10 +0400

    remove jest-formatting package

--- jest.js
+++ jest.js
@@ -1,25 +1,22 @@
 import jestPlugin from 'eslint-plugin-jest';
-import jestFormatting from 'eslint-plugin-jest-formatting';
 import importPlugin from "eslint-plugin-import";
 
 export default [
     jestPlugin.configs['flat/recommended'],
     {
-        files: jestFormatting.configs.recommended.overrides[0].files,
-        rules: jestFormatting.configs.recommended.overrides[0].rules,
-        plugins: {
-            "jest-formatting": jestFormatting,
-        },
-    },
-    {
         plugins: {
             jest: jestPlugin,
-            'jest-formatting': jestFormatting,
             'import': importPlugin
         },
         rules: {
             // Allow devDependencies to be imported when in development mode
             'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+            'jest/padding-around-after-all-blocks': 'error',
+            'jest/padding-around-after-each-blocks': 'error',
+            'jest/padding-around-before-all-blocks': 'error',
+            'jest/padding-around-before-each-blocks': 'error',
+            'jest/padding-around-describe-blocks': 'error',
+            'jest/padding-around-test-blocks': 'error'
         }
     }
 ];
